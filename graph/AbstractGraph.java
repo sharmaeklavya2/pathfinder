a6c93adacc67623609fcf1caca596bd88c7e3e41
Eklavya Sharma <ekurgn@gmail.com>
2017-03-25 04:38:00 +0530

    Improve graph

--- graph/AbstractGraph.java
+++ graph/AbstractGraph.java
@@ -2,10 +2,15 @@ package graph;
 
 import java.util.Map;
 
+import graph.Graph;
+
 public abstract class AbstractGraph
 {
     public abstract int size();
     public abstract Map<Integer, Double> getNbrs(int node);
     public abstract boolean adjacent(int src, int dst);
     public abstract double getWeight(int src, int dst);
+    public abstract String toString();
+
+    public abstract Graph getGraph();
 }
--- graph/Graph.java
+++ graph/Graph.java
@@ -8,6 +8,7 @@ import static java.util.Collections.unmodifiableMap;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 import graph.Edge;
@@ -15,13 +16,16 @@ import graph.AbstractGraph;
 
 public class Graph extends AbstractGraph
 {
-    private List<Map<Integer, Double>> adj;
+    private List<HashMap<Integer, Double>> adj;
 
     synchronized public int size() {
         return adj.size();
     }
     synchronized public Map<Integer, Double> getNbrs(int node) {
         return unmodifiableMap(adj.get(node));
+    }
+    synchronized public HashMap<Integer, Double> getNbrsCopy(int node) {
+        return new HashMap<Integer, Double>(adj.get(node));
     }
     synchronized public boolean adjacent(int src, int dst) {
         return (src < adj.size() && src >= 0 && adj.get(src).containsKey(dst));
@@ -33,45 +37,88 @@ public class Graph extends AbstractGraph
     public Graph(int n, Edge[] edges, boolean symmetric) {
         this(n);
 
-        for(Edge e: edges) {
-            update(e);
-            if(symmetric)
-                update(e.reverse());
-        }
-    }
-    public Graph(int n, Edge[] edges) {
-        this(n, edges, true);
+        for(Edge e: edges)
+            update(e, symmetric);
     }
     public Graph(int n) {
-        adj = new ArrayList<Map<Integer, Double>>(n);
+        adj = new ArrayList<HashMap<Integer, Double>>(n);
         for(int i=0; i<n; ++i)
             adj.add(new HashMap<Integer, Double>());
     }
+    public Graph(AbstractGraph graph2) {
+        this(graph2.size());
+        for(int u=0; u<adj.size(); ++u) {
+            for(Map.Entry<Integer, Double> entry: graph2.getNbrs(u).entrySet()) {
+                int v = entry.getKey();
+                double w = entry.getValue();
+                adj.get(u).put(v, w);
+            }
+        }
+    }
+
     synchronized public String toString() {
-        return "Graph(" + size() + ", " + adj + ")";
+        return "Graph(" + adj.size() + ", " + adj + ")";
     }
 
     synchronized public void update(Edge e) {
         adj.get(e.getSrc()).put(e.getDst(), e.getW());
     }
+    synchronized public void update(Edge e, boolean symmetric) {
+        update(e);
+        if(symmetric)
+            update(e.reverse());
+    }
+    synchronized public void breakEdge(int u, int v) {
+        adj.get(u).remove(v);
+    }
 
-    public static void main(String[] args) throws IOException
+    synchronized public Graph getGraph() {
+        return new Graph(this);
+    }
+    public static Graph fromBufferedReader(BufferedReader br, boolean symmetric) throws IOException
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String s;
         String[] words;
         Graph graph = new Graph(n);
-        while((s = br.readLine()) != null)
-        {
-            words = s.split(" ");
-            int src = Integer.parseInt(words[0]);
-            int dst = Integer.parseInt(words[1]);
-            double w = Double.parseDouble(words[2]);
-            Edge e = new Edge(src, dst, w);
-            graph.update(e);
-            graph.update(e.reverse());
+        while((s = br.readLine()) != null) {
+            if(!s.equals("")) {
+                words = s.split(" ");
+                int src = Integer.parseInt(words[0]);
+                int dst = Integer.parseInt(words[1]);
+                double w = Double.parseDouble(words[2]);
+                graph.update(new Edge(src, dst, w), symmetric);
+            }
         }
-        System.out.println(graph.toString());
+        return graph;
+    }
+
+    public static Graph fromFile(String path, boolean symmetric) throws IOException
+    {return fromBufferedReader(new BufferedReader(new FileReader(path)), symmetric);}
+
+    public static Graph fromStdin(boolean symmetric) throws IOException
+    {return fromBufferedReader(new BufferedReader(new InputStreamReader(System.in)), symmetric);}
+
+    public static void main(String[] args) throws IOException
+    {
+        String usage = "usage: java graphs.Graph [file]";
+        boolean symmetric = true;
+        Graph graph;
+        if(args.length == 0) {
+            graph = fromStdin(symmetric);
+            System.out.println(graph.toString());
+        }
+        else if(args.length == 1) {
+            if(args[0].equals("-h") || args[0].equals("--help"))
+                System.out.println(usage);
+            else {
+                graph = fromFile(args[0], symmetric);
+                System.out.println(graph.toString());
+            }
+        }
+        else {
+            System.err.println(usage);
+            System.exit(1);
+        }
     }
 }
